6da6485413fd3f197c9b3b31e21530757df25ca7
Chen Weizhi (@d2d.weizhi) <d2d.weizhi@gmail.com>
2025-02-05 17:28:11 +0800

    I need a break from tinkering

--- Article-03/kr-flow-demo/src/components/tasks/TaskList.tsx
+++ Article-03/kr-flow-demo/src/components/tasks/TaskList.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
-
-// Step 8: import Checkbox control from the kendo-react-inputs library.
+import React, { useState, useEffect } from 'react';
 import { Checkbox } from '@progress/kendo-react-inputs';
 
-// Step 1: We will add a simple prop definition for our individual Task items.
 interface Task {
 	id: string;
 	title: string;
 	completed: boolean;
 }
 
-// Step 2: Create a basic static list of Task items.
 const tasks: Task[] = [
 	{ id: "AB123", title: "Write article introduction", completed: false },
 	{ id: "AB1234", title: "Implement task list view", completed: false },
 	{ id: "AB1235", title: "Add checkbox functionality", completed: false },
 ];
 
-// Step 3: Create our TaskList component.
 export default function TaskList() {
+	const [taskList, setTaskList] = useState<Task[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState<Error | null>(null);
+	
+	// Perhaps we can add the useEffect hook right here.
+	
+	function onCompletionChange(event: any, taskId: string) {
+		setTaskList((prevTasks) =>
+			prevTasks.map((task) =>
+				task.id === taskId ? { ...task, completed: event.value } : task
+			)
+		);
+	}
+	
 	return (
 		<ul>
-			{/*Step 4: Loop over the array and populate our Task List.*/}
-			{tasks.map((task) => (
+			{taskList.map((task) => (
 				<li key={task.id}>
-					{/*{task.title}*/}
-					{/* Step 9: Update the list item with a Checkbox control */}
-					<Checkbox label={task.title} />
+					<Checkbox
+						label={task.title}
+						checked={task.completed}
+					  onChange={(event) => onCompletionChange(event, task.id)}
+					/>
 				</li>
 			))}
 		</ul>
