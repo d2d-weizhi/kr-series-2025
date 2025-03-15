5ff61ebd2ba0d18268189ca75cb08a5ba49b46e5
Chen Weizhi <d2d.weizhi@gmail.com>
2025-03-15 14:53:45 +0800

    Updated the Getting Started code with KR 2025 Q1

---
+++ Article-02/kr-flow-demo/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Calendar } from '@progress/kendo-react-dateinputs';
+import '@progress/kendo-theme-default/dist/all.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello KendoReact!</h1>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
