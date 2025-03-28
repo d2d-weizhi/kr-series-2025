5dde45e0e24a83f8801a958846b68490f3b0ad52
Chen Weizhi <d2d.weizhi@gmail.com>
2025-03-28 11:09:46 +0800

    Updated package.json & a basic component demo.

--- kr-flow-demo/src/app/page.tsx
+++ kr-flow-demo/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Calendar } from "@progress/kendo-react-dateinputs";
 
 export default function Home() {
   return (
@@ -25,7 +26,10 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
+        <Calendar
+          weekNumber={true}
+          showOtherMonth={true}
+        />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
